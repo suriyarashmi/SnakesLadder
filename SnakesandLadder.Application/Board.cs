1d1edaf799e596ad46528c7f26229edb06b57924
Rashmi Madhavan <rashmisen@yahoo.com>
2021-08-21 10:41:15 +0100

    Architectural Design for game

---
+++ SnakesandLadder.Application/Board.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakesandLadder.Application
+{
+    public class Board
+    {
+        private BoardPosition[] boardPosition;
+        private Snake[] snakeSet;
+        private Ladder[] ladderSet;
+
+    }
+}
