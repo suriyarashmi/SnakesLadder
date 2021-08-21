61761eeae66bb74e8ad559848bb650c73c9059e2
Rashmi Madhavan <rashmisen@yahoo.com>
2021-08-21 22:04:08 +0100

    Revert "Delete SnakesandLadder.Application directory"
    This reverts commit 64af03cefa4abaac16a4e78dbb23b10dc6ff13a9.

---
+++ SnakesandLadder.Application/Services/Dice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SnakesandLadder.Application.Models
+{
+    public class Dice
+    {
+        private int sides;
+
+        public Dice(int sides)
+        {
+            this.sides = sides;
+        }
+        //Constructor
+        public Dice()
+        {
+            this.sides = 6;
+        }
+
+        public int RollDice()
+        {
+            Random r = new Random();
+            int dices = r.Next(1, 7);
+            return dices;
+        }
+    }
+}
