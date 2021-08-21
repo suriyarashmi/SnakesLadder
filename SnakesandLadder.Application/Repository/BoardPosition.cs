5d5ce4309d5bd70bb9d4111196d045cef914ea0e
Rashmi Madhavan <rashmisen@yahoo.com>
2021-08-21 22:29:56 +0100

    Revert "Delete SnakesandLadder.Application directory"
    This reverts commit 64af03cefa4abaac16a4e78dbb23b10dc6ff13a9.

---
+++ SnakesandLadder.Application/Repository/BoardPosition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SnakesandLadder.Application
+{
+    public class BoardPosition
+    {
+        private int number;
+
+        public BoardPosition(int num)
+        {
+            this.number = num ;
+        }
+      
+        internal object GetSnakeLadder()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SetIsSnake(Snake s)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SetIsLadder(Ladder l)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
