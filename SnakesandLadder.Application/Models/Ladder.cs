84d12f5ecc73aa968d7288dd818611c0806e1f8c
Rashmi Madhavan <rashmisen@yahoo.com>
2021-08-21 22:04:08 +0100

    Revert "Delete SnakesandLadder.Application directory"
    This reverts commit 64af03cefa4abaac16a4e78dbb23b10dc6ff13a9.

---
+++ SnakesandLadder.Application/Models/Ladder.cs
@@ -0,0 +1,14 @@
+﻿namespace SnakesandLadder.Application
+{
+    class Ladder
+    {
+        private int end;
+        private int start;
+
+        public Ladder(int end, int start)
+        {
+            this.end = end;
+            this.start = start;
+        }
+    }
+}
