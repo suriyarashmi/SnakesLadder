9d45705a47d33836bfc9b3bd976b9ab46144039f
Rashmi Madhavan <rashmisen@yahoo.com>
2021-08-22 01:23:20 +0100

    New class Teleportar

---
+++ SnakesLadder.Application/Abstract/Teleportar.cs
@@ -0,0 +1,8 @@
+﻿namespace SnakesandLadder.Application.Abstract
+{
+    public abstract class Teleportar
+    {
+        private int tail, head;
+        public abstract void MovePlayer(Player p);
+    }
+}
