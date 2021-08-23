9d7e6c210ebee1336e24f3be32c8cc2945603c79
Rashmi Madhavan <rashmisen@yahoo.com>
2021-08-23 19:46:47 +0100

    Keeping project ready for Unit testing

---
+++ SnakesLadder/SnakesLadder.UnitTest/PlayGameTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SnakesLadder.UnitTest
+{
+    public class PlayGameTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
