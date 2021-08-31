06ddb8fad9871dee5a8c93487dba229a0fb2def1
Rashmi Madhavan <rashmisen@yahoo.com>
2021-08-31 07:44:54 +0100

    Added new unit Test on BoardPosition class

---
+++ SnakesLadder/SnakesLadder.UnitTest/BoardPositionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using SnakesLadder.Persistance;
+using SnakesLadder.Persistance.Repository;
+
+namespace SnakesLadder.UnitTest
+{
+    //Test Class
+   public class BoardPositionTest
+    {
+        //Test method
+        [Fact]
+        public void Find_BoardPosition()
+        {
+            //Arrange
+            const int num = 5;
+            BoardPosition Boardsquare = new BoardPosition(num) ;
+
+            Boardsquare.SetNumber(num);
+            //Act
+            var expected = num;
+             var actual =   Boardsquare.GetNumber();
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+            
+        
+    }
+}
